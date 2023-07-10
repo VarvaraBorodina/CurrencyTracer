eb0f369ca44f196fbb26891ec15e38b895ca34be
Varvara Borodina <vava.borodina@mail.ru>
2023-07-10 13:40:44 +0300

    feat: add getting map data from API

--- src/constants/banksWithCurrencies.ts
+++ src/constants/banksWithCurrencies.ts
@@ -1,13 +1,62 @@
 const BANKS_WITH_CURRENCIES = {
-  USD: [1337, 1338, 1224],
-  ARS: [1339, 1340, 1226],
-  GBP: [1341, 1192, 1232],
-  BTC: [1199, 1201, 1340],
-  AUD: [1202, 1203, 1192],
-  CNY: [1204, 1205, 1234],
-  JPY: [1207, 1208, 1243],
-  CAD: [1213, 1219, 1246],
-  EUR: [1220, 1222, 1248],
+  USD: [
+    '515ee659e4b04e50b05db307',
+    '4f548dafe4b03365364c1416',
+    '4ed72c3582311e04d1f9a574',
+    '4e145f7f62e14518a924a80a',
+  ],
+  ARS: [
+    '4ed72c3582311e04d1f9a574',
+    '4e8adfd1cc215b0368311ff3',
+    '500eae7ee4b07f8ff6345e36',
+    '4ed49c5b5c5c6e2492c8972c',
+  ],
+  GBP: [
+    '514075cde4b09ccb3c622623',
+    '500eae7ee4b07f8ff6345e36',
+    '4ed49c5b5c5c6e2492c8972c',
+    '4ed49c5b5c5c6e2492c8972c',
+  ],
+  BTC: [
+    '515ee659e4b04e50b05db307',
+    '4f548dafe4b03365364c1416',
+    '4ed72c3582311e04d1f9a574',
+    '514075cde4b09ccb3c622623c',
+    '4ed49c5b5c5c6e2492c8972c',
+  ],
+  AUD: [
+    '4f548dafe4b03365364c1416',
+    '514075cde4b09ccb3c622623',
+    '4e8adfd1cc215b0368311ff3',
+    '4e145f7f62e14518a924a80a',
+  ],
+  CNY: [
+    '515ee659e4b04e50b05db307',
+    '4ed72c3582311e04d1f9a574s',
+    '514075cde4b09ccb3c622623',
+    '4e145f7f62e14518a924a80a',
+  ],
+  JPY: [
+    '4e8adfd1cc215b0368311ff3',
+    '500eae7ee4b07f8ff6345e36',
+    '4ed49c5b5c5c6e2492c8972c',
+    '4e2552f452b17b2b5dccfb14',
+    '5d63853a4c433b00084ac27a',
+  ],
+  CAD: [
+    '515ee659e4b04e50b05db307',
+    '514075cde4b09ccb3c622623',
+    '500eae7ee4b07f8ff6345e36',
+    '4e2552f452b17b2b5dccfb14',
+    '5d63853a4c433b00084ac27a',
+  ],
+  EUR: [
+    '4f548dafe4b03365364c1416',
+    '4ed72c3582311e04d1f9a574',
+    '4e8adfd1cc215b0368311ff3',
+    '4e2552f452b17b2b5dccfb14',
+    '5d63853a4c433b00084ac27a',
+  ],
 }
 
 export default BANKS_WITH_CURRENCIES
